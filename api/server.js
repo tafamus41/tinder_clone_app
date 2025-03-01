814b6baa9671900badcfe900f8d6db8701d94f7a
asimov <seyranmustafa4@gmail.com>
2025-03-02 01:39:10 +0300

    p

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cors from "cors";
 // import path from "path";
 import { createServer } from "http";
 
@@ -26,12 +26,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-// 	cors({
-// 		origin: process.env.CLIENT_URL,
-// 		credentials: true,
-// 	})
-// );
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+		credentials: true,
+	})
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
