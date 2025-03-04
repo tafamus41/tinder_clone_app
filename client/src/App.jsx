13d00ef52866761430215f015bf5abd764bb421f
asimov <seyranmustafa4@gmail.com>
2025-03-05 00:44:49 +0300

    p

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 		<div className='absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]'>
 			<Routes>
 				<Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/auth"} />} />
-				<Route path='/auth' element={!authUser ? <AuthPage /> : <Navigate to={"/"} />} />
+				<Route path='/auth' element={authUser ? <AuthPage /> : <Navigate to={"/"} />} />
 				<Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to={"/auth"} />} />
 				<Route path='/chat/:id' element={authUser ? <ChatPage /> : <Navigate to={"/auth"} />} />
 			</Routes>
