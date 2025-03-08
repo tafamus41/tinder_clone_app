723714e240e1d0995921ccf9ba703c959012af5f
asimov <seyranmustafa4@gmail.com>
2025-03-08 03:34:22 +0300

    p

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export const Header = () => {
 					<div className='flex items-center'>
 						<Link to='/' className='flex items-center space-x-2'>
 							<Flame className='w-8 h-8 text-white' />
-							<span className='text-2xl font-bold text-white hidden sm:inline'>Swipe</span>
+							<span className='text-2xl font-bold text-white hidden sm:inline'>FINDER</span>
 						</Link>
 					</div>
 
