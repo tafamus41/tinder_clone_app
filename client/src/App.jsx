1e139c9bbd179e01f0174633be060c48691e6a66
asimov <seyranmustafa4@gmail.com>
2025-03-08 03:26:01 +0300

    p

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage";
 import ChatPage from "./pages/ChatPage";
@@ -20,7 +20,8 @@ function App() {
 	return (
 		<div className='absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]'>
 			<Routes>
-				<Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/auth"} />} />
+				{/* <Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/auth"} />} /> */}
+				<Route path='/' element={<AuthPage />} />
 				<Route path='/auth' element={!authUser ? <AuthPage /> : <Navigate to={"/"} />} />
 				<Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to={"/auth"} />} />
 				<Route path='/chat/:id' element={authUser ? <ChatPage /> : <Navigate to={"/auth"} />} />
